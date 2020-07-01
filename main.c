0fdf337644d2299048f6ae28fbf14c9e209553c3
zyx <1250897150@qq.com>
2020-07-01 20:46:27 +0800

    draw suantoujun

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("xxxx  xxx                      x \n");
+    printf("xxxxxxxxxxxxx                  xx\n");
+    printf("  xxxxxxxx                    xxxx\n");
+    printf("     xxxxxxxxxxxxxxxxxxxxxxx   x x\n");
+    printf("    xx xxx                  xxxxxx\n");
+    printf("  xxxxxx                      xx\n");
+    printf("xxxxxx                         xxx\n");
+    printf("    xx                          xx\n");
+    printf("   xx     x             x x      xx\n");
+    printf("  xx     xxx             x        xx\n");
+    printf(" xx     xxxxx           x x        xx\n");
+    printf("xx              xx                  x\n");
+    printf("x               xx                   x\n");
+    printf("x                                     x\n");
+    printf("x               xxxx                  x\n");
+    printf("x                                     x\n");
+    printf("xxx                                  x\n");
+    printf("  xxx                              xx\n");
+    printf("   xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n"); 
+    
+
+ 
+}
